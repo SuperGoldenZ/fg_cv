a528593154704c28ab9f2d3dc440c3f29a0e32fb
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-08-10 19:52:22 +0900

    first commit

---
+++ src/fg_cv/__init__.py
@@ -0,0 +1,12 @@
+"""
+fg_cv: A library for extracing information from fighting games
+"""
+
+__title__ = "fg_cv"
+__author__ = "Alexander Golden"
+__license__ = "MIT"
+__js__ = None
+__js_url__ = None
+
+from fg_cv.vs_screen_extractor_base import VsScreenExtractorBase
+from fg_cv.extractor_factory import ExtractorFactory
