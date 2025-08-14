15db4088e68fbf8f417e7a3ac9c3c173a38edc1b
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-08-14 21:15:52 +0900

    sf6 vs screen layout

---
+++ src/fg_cv/games/sf6/layouts/vs.py
@@ -0,0 +1,9 @@
+# General
+layout = {
+    "p1_character" : {"x": 101, "y": 824, "w": 285, "h":60},
+    "p2_character" : {"x": 1532, "y": 824, "w": 285, "h":60},
+    "fixed_colors" : [
+        {"x": 900, "y": 297, "color": "#a100fd"},
+        {"x": 1020, "y": 434, "color": "#a905fe"}
+    ]
+}
