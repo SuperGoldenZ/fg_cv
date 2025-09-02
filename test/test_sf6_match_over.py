df165658a16026daaf98d6dbf9026d6edc22b56c
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-09-02 23:55:22 +0900

    updating tests

--- test/test_sf6_match_over.py
+++ test/test_sf6_match_over.py
@@ -1,23 +1,42 @@
 import sys
 import os
 import cv2
+import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 from fg_cv.vs_screen_extractor import VsScreenExtractor
 from fg_cv.match_over_extractor import MatchOverExtractor
-        
-def test_is_match_over():
-    mo = MatchOverExtractor(game="sf6")
-    frame = cv2.imread("assets/test_images/match_over.png")
-    mo.set_frame(frame)    
-    assert(mo.is_match_over_screen())    
-    
-    mo = MatchOverExtractor(game="sf6")
-    frame = cv2.imread("assets/test_images/match_over_02.png")
-    mo.set_frame(frame)    
-    assert(mo.is_match_over_screen())    
 
-    frame = cv2.imread("assets/test_images/cammy_vs_elena.png")
-    mo.set_frame(frame)    
-    assert(not mo.is_match_over_screen())
+match_over_test_data = [
+    "assets/test_images/sf6/match_over_01.png",
+    "assets/test_images/sf6/match_over_02.png",
+    "assets/test_images/match_over.png",
+    "assets/test_images/match_over_02.png",
+]
+
+
+@pytest.mark.parametrize("filename", match_over_test_data)
+def test_is_match_over(filename):
+    mo = MatchOverExtractor(game="sf6")
+    frame = cv2.imread(filename)
+    mo.set_frame(frame)
+    assert mo.is_match_over_screen()
+
+
+not_match_over_test_data = [
+    "assets/test_images/sf6/not_match_over_01.png",
+    "assets/test_images/sf6/not_match_over_02.png",
+    "assets/test_images/sf6/not_match_over_03.png",
+    "assets/test_images/sf6/not_match_over_04.png",
+    "assets/test_images/mai_vs_juri.png",
+]
+
+
+@pytest.mark.parametrize("filename", not_match_over_test_data)
+def test_not_match_over(filename: str):
+    print(filename)
+    mo = MatchOverExtractor(game="sf6")
+    frame = cv2.imread(filename)
+    mo.set_frame(frame)
+    assert not mo.is_match_over_screen()
