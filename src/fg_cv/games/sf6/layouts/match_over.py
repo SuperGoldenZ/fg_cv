0cd2d6439e6e0df6cf5e6efe7c9026489ee74303
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-09-13 23:22:22 +0900

    updating to new format

--- src/fg_cv/games/sf6/layouts/match_over.py
+++ src/fg_cv/games/sf6/layouts/match_over.py
@@ -1,5 +1,5 @@
 layout = {
-    "fixed_colors": [
+    "expected_colors": [
         {"x": 606, "y": 472, "color": "#08233b"},
         {"x": 864, "y": 517, "color": "#edf6fa"},
         {"x": 956, "y": 564, "color": "#a1a1a0"},
@@ -9,4 +9,5 @@ layout = {
         {"x": 1306, "y": 482, "color": "#072443"},
         {"x": 894, "y": 623, "color": "#091f36"},
     ],
+    "threshold": 6,
 }
