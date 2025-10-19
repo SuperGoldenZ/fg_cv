73101df0b70ad52b9012402c46e6e0e324b8c3d3
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-10-19 14:29:02 +0900

    format code and fix first 99

--- src/fg_cv/video_cv.py
+++ src/fg_cv/video_cv.py
@@ -1,6 +1,7 @@
 import cv2
 
 from fg_cv.vs_screen_extractor import VsScreenExtractor
+from fg_cv.flexible_cv import FlexibleCv
 
 
 class VideoCv:
@@ -20,7 +21,7 @@ class VideoCv:
 
         frame_index = 0
         fps = cap.get(cv2.CAP_PROP_FPS)
-        
+
         result = None
 
         while True:
@@ -33,6 +34,43 @@ class VideoCv:
             vs.set_frame(frame)
 
             if vs.is_vs_screen():
+                result = frame
+                break
+
+            frame_index += 1
+
+        cap.release()
+        return result
+
+    @staticmethod
+    def find_first_99(video_path, fc: FlexibleCv):
+        """
+        Reads a local video file frame-by-frame using OpenCV and processes each frame.
+
+        Parameters:
+            video_path (str): Path to the local video file.
+            process_frame_callback (function): Function to call for each frame.
+                                            Signature: func(frame, frame_index, timestamp_sec)
+        """
+        cap = cv2.VideoCapture(video_path)
+        if not cap.isOpened():
+            raise FileNotFoundError(f"Could not open video file: {video_path}")
+
+        frame_index = 0
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        result = None
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break  # No more frames
+
+            timestamp_sec = frame_index / fps
+
+            fc.set_frame(frame)
+
+            if fc.is_match():
                 result = frame
                 break
 
