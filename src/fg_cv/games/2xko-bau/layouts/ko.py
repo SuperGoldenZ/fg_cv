fc82283fedfcc31650731b619aa2d09a393e0509
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-11-29 15:52:15 +0900

    2xko-bau

---
+++ src/fg_cv/games/2xko-bau/layouts/ko.py
@@ -0,0 +1,10 @@
+# General
+layout = {
+    "expected_colors": [
+        {"x": 606, "y": 429, "color": "#d7fd73"},
+        {"x": 208, "y": 241, "color": "#ffffff"},
+        {"x": 1331, "y": 536, "color": "#d7fd72"}
+    ],
+    "unexpected_colors": [],
+    "threshold": 3,
+}
