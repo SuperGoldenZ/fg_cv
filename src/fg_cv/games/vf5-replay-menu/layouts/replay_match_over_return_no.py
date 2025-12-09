0ffc92b97a0177d44b671ed41fa34f0233b37e09
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-12-09 21:40:27 +0900

    more layouts

---
+++ src/fg_cv/games/vf5-replay-menu/layouts/replay_match_over_return_no.py
@@ -0,0 +1,11 @@
+# General
+layout = {
+    "expected_colors": [
+        {"x": 599, "y": 416, "color": "#c10000"},
+        {"x": 689, "y": 590, "color": "#2e2e2e"},
+        {"x": 1061, "y": 590, "color": "#ee8b02"},
+        {"x": 1286, "y": 662, "color": "#0e24e0"},
+    ],
+    "unexpected_colors": [],
+    "threshold": 4,
+}
