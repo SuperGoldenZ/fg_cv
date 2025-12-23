d85c7b3320114432ee70861d68253dcf7b3e393a
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-12-23 20:30:45 +0900

    adding test cases

--- test/test_vf5_ranking_replay_download_complete.py
+++ test/test_vf5_ranking_replay_download_complete.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../s
 from fg_cv.flexible_cv import FlexibleCv
 
 
-match_over_test_data = [
+downloading_complete_data = [
     ["assets/test_images/vf5/ranked_replay_download_complete.png", True],
     ["assets/test_images/vf5/ranking_player_data_ids.png", False],
     ["assets/test_images/vf5/ranking_player_data_ids_01.png", False],
@@ -16,11 +16,30 @@ match_over_test_data = [
 ]
 
 
-@pytest.mark.parametrize("filename,expected", match_over_test_data)
-def test_is_match_over(filename, expected):
+@pytest.mark.parametrize("filename,expected", downloading_complete_data)
+def test_is_downloading_complete(filename, expected):
     cv: FlexibleCv = FlexibleCv(
         game="vf5", layout_name="ranking_replay_download_complete"
     )
+
+    assert os.path.isfile(filename)
+    frame = cv2.imread(filename)
+    cv.set_frame(frame)
+    assert cv.is_match() == expected
+
+
+downloading_data = [
+    ["assets/test_images/vf5/ranked_replay_downloading.png", True],
+    ["assets/test_images/vf5/ranked_replay_download_complete.png", False],
+    ["assets/test_images/vf5/ranking_player_data_ids.png", False],
+    ["assets/test_images/vf5/ranking_player_data_ids_01.png", False],
+    ["assets/test_images/vf5/ranking_player_data_ids_02.png", False],
+]
+
+
+@pytest.mark.parametrize("filename,expected", downloading_data)
+def test_is_downloading(filename, expected):
+    cv: FlexibleCv = FlexibleCv(game="vf5", layout_name="ranked_replay_downloading")
 
     assert os.path.isfile(filename)
     frame = cv2.imread(filename)
