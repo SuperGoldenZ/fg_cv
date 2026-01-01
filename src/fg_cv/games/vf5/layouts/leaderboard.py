877e15a9e037b8c7e9bd1b6cd54b9895c46a7f1b
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2026-01-01 10:31:54 +0900

    new additions for VF5

---
+++ src/fg_cv/games/vf5/layouts/leaderboard.py
@@ -0,0 +1,16 @@
+layout = {
+    "expected_colors": [
+        {"x": 62, "y": 99, "color": "#e9e9e9"},
+        {"x": 845, "y": 108, "color": "#000000"},
+        {"x": 502, "y": 83, "color": "#ffffff"},
+        {"x": 513, "y": 128, "color": "#9b9b9b"},
+        {"x": 1397, "y": 57, "color": "#001fff"},
+    ],
+    "unexpected_colors": [
+        {"x": 710, "y": 81, "color": "#bf0000"}
+        # {"x": 933, "y": 498, "color": "#898989"},
+        # {"x": 921, "y": 584, "color": "#898989"},
+        # {"x": 833, "y": 513, "color": "#0d0d0d"},
+    ],
+    "threshold": 5,
+}
