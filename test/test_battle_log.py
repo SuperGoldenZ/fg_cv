3b6000d140ab9c22b3dc3b2d0b13f01f3fc46840
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2026-01-08 21:20:17 +0900

    new test data

--- test/test_battle_log.py
+++ test/test_battle_log.py
@@ -18,8 +18,10 @@ match_over_test_data = [
 ]
 
 
-@pytest.mark.parametrize("filename,match1_datetime,match2_datetime,match3_datetime", match_over_test_data)
-def test_is_match_over(filename, match1_datetime, match2_datetime, match3_datetime):
+@pytest.mark.parametrize(
+    "filename,match1_datetime,match2_datetime,match3_datetime", match_over_test_data
+)
+def test_is_battle_log(filename, match1_datetime, match2_datetime, match3_datetime):
     cv: FlexibleCv = FlexibleCv(game="vf5", layout_name="battle_log")
 
     assert os.path.isfile(filename)
@@ -28,9 +30,39 @@ def test_is_match_over(filename, match1_datetime, match2_datetime, match3_dateti
     assert cv.is_match()
     ocr = cv.get_ocr_blocks()
 
-    assert(match1_datetime == ocr["match1_datetime"])
-    assert(match2_datetime == ocr["match2_datetime"])
-    assert(match3_datetime == ocr["match3_datetime"])
+    assert match1_datetime == ocr["match1_datetime"]
+    assert match2_datetime == ocr["match2_datetime"]
+    assert match3_datetime == ocr["match3_datetime"]
 
     print(ocr)
 
+
+battle_log_no_matches_data = [
+    [
+        "player_data_battle_log_selected_no_matches",
+        "assets/test_images/vf5/player_data_no_battle_log.png",
+        True,
+    ],
+    [
+        "player_data_battle_log_selected_no_matches",
+        "assets/test_images/vf5/ranking_battle_log_01_selected.png",
+        False,
+    ],
+    [
+        "battle_log",
+        "assets/test_images/vf5/player_data_no_battle_log.png",
+        False,
+    ],
+]
+
+
+@pytest.mark.parametrize(
+    "layout_name, test_image_filename, expected_result", battle_log_no_matches_data
+)
+def test_battle_log_no_matches(layout_name, test_image_filename, expected_result):
+    cv: FlexibleCv = FlexibleCv(game="vf5", layout_name=layout_name)
+
+    assert os.path.isfile(test_image_filename)
+    frame = cv2.imread(test_image_filename)
+    cv.set_frame(frame)
+    assert cv.is_match() == expected_result
