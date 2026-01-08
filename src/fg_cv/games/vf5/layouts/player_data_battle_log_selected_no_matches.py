790b94886a8428a911bf2e3eb28ceda67721be31
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2026-01-08 21:21:18 +0900

    detect when no matches in battle log

---
+++ src/fg_cv/games/vf5/layouts/player_data_battle_log_selected_no_matches.py
@@ -0,0 +1,23 @@
+# player_data_battle_log_selected_no_matches
+layout = {
+    "expected_colors": [
+        {"x": 60, "y": 101, "color": "#e6e6e6"},
+        {"x": 828, "y": 551, "color": "#302d31"},
+        {"x": 828, "y": 451, "color": "#302d31"},
+        {"x": 828, "y": 651, "color": "#302d31"},
+        {"x": 828, "y": 651, "color": "#302d31"},
+        {"x": 465, "y": 525, "color": "#e48302"},
+        {"x": 318, "y": 123, "color": "#ffffff"},
+        {"x": 1608, "y": 908, "color": "#1e1c1f"},
+        {"x": 1608, "y": 908, "color": "#1e1c1f"},
+        {"x": 767, "y": 929, "color": "#0e24e0"},
+        {"x": 440, "y": 927, "color": "#0e24e0"},
+        {"x": 990, "y": 503, "color": "#302d31"},
+        {"x": 1095, "y": 473, "color": "#919192"},
+        {"x": 1536, "y": 900, "color": "#1e1c1f"},
+        {"x": 1494, "y": 735, "color": "#302d31"},
+        {"x": 1197, "y": 434, "color": "#302d31"},
+    ],
+    "unexpected_colors": [{"x": 1353, "y": 552, "color": "#000000"}],
+    "threshold": 14,
+}
